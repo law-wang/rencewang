0fa49ec42cea3c98fe1be8a27543b9d7e44bece7
law-wang <47406212+law-wang@users.noreply.github.com>
2020-02-15 17:59:31 -0500

    changed about page

--- assets/scripts/landing.js
+++ assets/scripts/landing.js
@@ -20,3 +20,8 @@ function showAbout() {
     x.style.display = "none";
   }
 }
+function showContact() {
+  document.getElementById("contact").innerHTML = "<a href='https://www.facebook.com/lawrencio.w'>FACEBOOK</a><br><a href='https://www.instagram.com/lawrencio.w/'>INSTAGRAM</a><br><a href='https://www.linkedin.com/in/lawrence-c-w/'>LINKEDIN</a><br><a href='https://open.spotify.com/user/2232mfikorkepi3hqhfpkgdhq?si=V1tFp8yNSGatvckEe4Ia8w'>SPOTIFY</a><br>";
+  document.getElementById("ct").innerHTML = "王晨玮";
+
+}
