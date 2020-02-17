18a540901f942c016fe336cb8d21c8b42d73254c
law-wang <47406212+law-wang@users.noreply.github.com>
2020-02-16 20:29:44 -0500

    tried cursor

--- assets/scripts/landing.js
+++ assets/scripts/landing.js
@@ -22,7 +22,7 @@ function showAbout() {
 }
 function showContact() {
   document.getElementById("contact").innerHTML = "<a href='https://www.facebook.com/lawrencio.w'>FACEBOOK</a><br><a href='https://www.instagram.com/lawrencio.w/'>INSTAGRAM</a><br><a href='https://www.linkedin.com/in/lawrence-c-w/'>LINKEDIN</a><br><a href='https://open.spotify.com/user/2232mfikorkepi3hqhfpkgdhq?si=V1tFp8yNSGatvckEe4Ia8w'>SPOTIFY</a><br>";
-  document.getElementById("ct").innerHTML = "王晨玮";
+  //document.getElementById("ct").innerHTML = "王晨玮";
 }
 function greeting() {
   var day = new Date();
