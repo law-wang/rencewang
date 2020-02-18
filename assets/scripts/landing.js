e451e2042ea9b06bc205e784d38527157b275a4c
law-wang <47406212+law-wang@users.noreply.github.com>
2020-02-18 14:44:49 -0500

    added project button

--- assets/scripts/landing.js
+++ assets/scripts/landing.js
@@ -24,6 +24,15 @@ function showContact() {
   document.getElementById("contact").innerHTML = "<a href='https://www.facebook.com/lawrencio.w'>FACEBOOK</a><br><a href='https://www.instagram.com/lawrencio.w/'>INSTAGRAM</a><br><a href='https://www.linkedin.com/in/lawrence-c-w/'>LINKEDIN</a><br><a href='https://open.spotify.com/user/2232mfikorkepi3hqhfpkgdhq?si=V1tFp8yNSGatvckEe4Ia8w'>SPOTIFY</a><br>";
   //document.getElementById("ct").innerHTML = "王晨玮";
 }
+function showProject() {
+  var x = document.getElementById("project");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }
+  else {
+    x.style.display = "none";
+  }
+}
 function greeting() {
   var day = new Date();
   var hr = day.getHours();
