3a8d55d774cff73b715ae1b8cb5068cb160884d7
law-wang <47406212+law-wang@users.noreply.github.com>
2020-02-20 00:19:13 -0500

    Update landing.js

--- assets/scripts/landing.js
+++ assets/scripts/landing.js
@@ -37,7 +37,7 @@ function showAboutMobile() {
   }
 }
 function showContact() {
-  document.getElementById("contact").innerHTML = "<a href='https://www.facebook.com/lawrencio.w'>FACEBOOK</a><br><a href='https://www.instagram.com/lawrencio.w/'>INSTAGRAM</a><br><a href='https://www.linkedin.com/in/lawrence-c-w/'>LINKEDIN</a><br><a href='https://open.spotify.com/user/2232mfikorkepi3hqhfpkgdhq?si=V1tFp8yNSGatvckEe4Ia8w'>SPOTIFY</a><br>";
+  document.getElementById("contact").innerHTML = "<a href='https://www.facebook.com/lawrencio.w' target='_blank'>FACEBOOK</a><br><a href='https://www.instagram.com/lawrencio.w/' target='_blank'>INSTAGRAM</a><br><a href='https://www.linkedin.com/in/lawrence-c-w/' target='_blank'>LINKEDIN</a><br><a href='https://open.spotify.com/user/2232mfikorkepi3hqhfpkgdhq?si=V1tFp8yNSGatvckEe4Ia8w' target='_blank'>SPOTIFY</a><br>";
   //document.getElementById("ct").innerHTML = "王晨玮";
 }
 function showProject() {
